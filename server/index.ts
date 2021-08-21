28d9178851f195106c9ede5cfdb37242b79aeaf3
matan <matan.noach@gmail.com>
2021-08-21 18:26:26 +0300

    adding complex search options

--- server/index.ts
+++ server/index.ts
@@ -24,14 +24,108 @@ app.use((_, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
-function filterTickets(tickets:Ticket[],search:string){
-  if(search!==""){
-  return tickets.filter((t) =>
-      (t.title.toLowerCase() + t.content.toLowerCase()).includes(
-        search.toLowerCase()
-      )
-    ); 
-  }else{
+/**
+ * The type represents the search values given by the user
+ * values:
+ * search - The string to search in the text
+ * bDate - Get tickets before this date
+ * aDate - Get tickets after this date
+ * from - Get tickets from this email
+ * labels - Get tickets with these labels
+ * 
+ * example - before:31/08/2021 after:01/08/2021 from:matan.noach@gmail.com labels:corvid,Guidelines hello world
+ * will create the following object: 
+ * {search: "hello world", bDate:31/08/2021, aDate:01/08/2021, from:matan.noach@gmail.com, labels:['corvid','Guidelines'] }
+ */
+type SearchValues={
+  search:string;
+  bDate?:number;
+  aDate?:number;
+  from?:string;
+  labels?:string[];
+};
+/**
+ * The function parses a search line, parses it and returns the SearchValues objects represents it 
+ * @param search - The search line
+ * @returns - A SearchValues object
+ */
+function parseSearch(search:string):SearchValues{
+  var searchValues:SearchValues = {search:""};
+  // parse the line by " " to get the search options
+  const options: string[] = search.split(" ");
+  search = "";
+  // for each option in the line
+  for (let option of options) {
+    // try splitting each option with ':'
+    const values: string[] = option.split(':');
+    // if there are 2 values found - might be an option:value
+    if (values.length == 2) {
+      // check which option it is and add it to the object
+      if (values[0] === "before") {
+        searchValues.bDate = new Date(values[1]).getTime();
+      } else if (values[0] === "after") {
+        searchValues.aDate = new Date(values[1]).getTime();
+      } else if (values[0] === "from") {
+        searchValues.from = values[1];
+      } else if (values[0] === "labels") {
+        searchValues.labels = values[1].split(",");
+      }else{
+        // if none of the above, then it's part of the search
+        values.map((word) => (search = search.concat(word+":")));
+        search = search.slice(0, search.length - 1);
+        search+=" ";
+      }
+    } else {
+      values.map((word) => (search = search.concat(word + " ")));
+    }
+  }
+  // slice the last " " from the search
+  searchValues.search = search.slice(0, search.length - 1);
+  console.log(searchValues)
+  return searchValues;
+}
+/**
+ * The function gets a search line and filters the tickets by it
+ * @param tickets - An array of tickets to filter
+ * @param search - The search line given by the user
+ * @returns - A filtered tickets
+ * NOTE - if there is no search inputs, returns the original tickets
+ */
+function filterTickets(tickets: Ticket[], search: string) {
+  if (search !== "") {
+    // parse the search line into SearchValues object
+    const searchValues:SearchValues = parseSearch(search);
+    // filter the tickets by SearchValues
+    return tickets.filter((t) => {
+      var flag: Boolean = true;
+      if (
+        (t.title.toLowerCase() + t.content.toLowerCase()).includes(
+          searchValues.search.toLowerCase()
+        )
+      ) {
+        if (searchValues.bDate && t.creationTime > searchValues.bDate) {
+          flag = false;
+        }
+        if (searchValues.aDate && t.creationTime < searchValues.aDate) {
+          flag = false;
+        }
+        if (searchValues.from && t.userEmail !== searchValues.from) {
+          flag = false;
+        }
+        if (searchValues.labels) {
+          if (
+            !t.labels ||
+            !searchValues.labels.every((label) => t.labels?.includes(label))
+          ) {
+            flag = false;
+          }
+        }
+        if (flag) {
+          return t;
+        }
+      }
+    });
+  } else {
     return tickets;
   }
 }
@@ -76,25 +170,24 @@ function sortTickets(tickets: Ticket[], sortBy: string, asc: number) {
  * The function also sends the max number of pages and the data size
  */
 app.get(APIPath, (req, res) => {
-  // extract the values from the URL
+  // extract the values from the URI
   const page: number = parseInt(req.query.page as string);
   const sortBy: string = req.query.sortBy as string;
   const asc: number = parseInt(req.query.asc as string);
-  const search:string = req.query.search as string;
-  console.log(req.query)
+  const search: string = req.query.search as string;
   // filter the data by search option
-  const filteredData = filterTickets(tempData,search);
+  const filteredData = filterTickets(tempData, search);
   // sort the data
   const sortedData = sortTickets(filteredData, sortBy, asc);
   // get the data size and max number of pages
   const dataSize = sortedData.length;
   const maxPage = Math.ceil(sortedData.length / PAGE_SIZE);
-  // get the page slice from the data
+  // slice a page from the data
   const paginatedData = sortedData.slice(
     (page - 1) * PAGE_SIZE,
     page * PAGE_SIZE
   );
-  // return a json file of the data, whole data size and max page number
+  // return a json file of the data, data size and max page number
   res.json({ data: paginatedData, dataSize: dataSize, maxPage: maxPage });
 });
 
@@ -146,44 +239,37 @@ app.put(APIPath + "/:id/labels/:label", (req, res) => {
     // if the ticket have an existing labels array
     if (t.labels) {
       t.labels.push(label);
-    }else{ // create a new array with new label
-      t.labels = [label]
+    } else {
+      // create a new array with new label
+      t.labels = [label];
     }
     // write the new data to the DB and send the label array to the client on success
-    fs.writeFile(
-      fileName,
-      JSON.stringify(tempData, null, 2),
-      (err: string) => {
-        if (err) throw err;
-        console.log("file has been updated!");
-        res.send(t.labels);
-      }
-    );
+    fs.writeFile(fileName, JSON.stringify(tempData, null, 2), (err: string) => {
+      if (err) throw err;
+      console.log("file has been updated!");
+      res.send(t.labels);
+    });
   } else {
     console.log("Ticket was not found!");
   }
 });
-app.put(APIPath+"/:id/title/:title",(req,res)=>{
+app.put(APIPath + "/:id/title/:title", (req, res) => {
   // extract id and label the data from the URL
   const id: string = req.params.id as string;
   const title: string = req.params.title as string;
   // find the ticket by id
   const t = tempData.find((t) => t.id === id);
-  if(t){
+  if (t) {
     t.title = title;
     // write the new data to the DB and send the title to the client on success
-    fs.writeFile(
-      fileName,
-      JSON.stringify(tempData, null, 2),
-      (err: string) => {
-        if (err) throw err;
-        console.log("file has been updated!");
-        res.send(t.title);
-      }
-    );
+    fs.writeFile(fileName, JSON.stringify(tempData, null, 2), (err: string) => {
+      if (err) throw err;
+      console.log("file has been updated!");
+      res.send(t.title);
+    });
   } else {
     console.log("Ticket was not found!");
   }
-})
+});
 app.listen(serverAPIPort);
 console.log("server running", serverAPIPort);
